274eb8b9607cf660968ca42a41ea448711d63760
Uyen <uyen.nguyenminhduy@gmail.com>
2022-07-21 14:27:06 +0700

    Initial Commit

---
+++ MyMovieList/MyMovieListApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMovieListApp.swift
+//  MyMovieList
+//
+//  Created by Uyen Nguyen Minh Duy on 21/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct MyMovieListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
