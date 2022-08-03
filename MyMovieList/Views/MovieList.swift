f49d1af53d4fa861e941fd5e586998c66860e838
IDS-V-Intern-Uyennmd <67620752+uyennmd@users.noreply.github.com>
2022-08-03 12:21:48 +0700

    Update MovieList.swift

--- MyMovieList/Views/MovieList.swift
+++ MyMovieList/Views/MovieList.swift
@@ -1,9 +1,14 @@
-//
-//  MyMovieList.swift
-//  MyMovieList
-//
-//  Created by Uyen Nguyen Minh Duy on 27/07/2022.
-//
+/*
+  RMIT University Vietnam
+  Course: COSC2659 iOS Development
+  Semester: 2022B
+  Assessment: Assignment 1
+  Author: Nguyen Minh Duy Uyen
+  ID: s3819342
+  Created  date: 25/07/2022
+  Last modified: 03/08/2022
+  Acknowledgement: Acknowledge the resources that you use here. 
+*/
 
 import Foundation
 import SwiftUI
